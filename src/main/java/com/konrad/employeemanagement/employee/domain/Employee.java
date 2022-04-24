75762c62e76c167427f1ca573406c95472426d8a
konradsiekacz <konradsiekacz@gmail.com>
2022-04-24 20:08:15 +0200

    create fields

--- src/main/java/com/konrad/employeemanagement/employee/domain/Employee.java
+++ src/main/java/com/konrad/employeemanagement/employee/domain/Employee.java
@@ -1,4 +1,16 @@
 package com.konrad.employeemanagement.employee.domain;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Employee {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
