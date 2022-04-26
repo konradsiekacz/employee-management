f3b339d2f1ba8eeb3e703aba405e95e65a31bdb9
konradsiekacz <konradsiekacz@gmail.com>
2022-04-26 23:03:03 +0200

    add method to get all employees

--- src/main/java/com/konrad/employeemanagement/employee/EmployeeController.java
+++ src/main/java/com/konrad/employeemanagement/employee/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.konrad.employeemanagement.employee;
 
+import com.konrad.employeemanagement.employee.domain.Employee;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+@AllArgsConstructor
 public class EmployeeController {
+    private EmployeeService employeeService;
+
+
+    @GetMapping("")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
 }
--- src/main/java/com/konrad/employeemanagement/employee/EmployeeService.java
+++ src/main/java/com/konrad/employeemanagement/employee/EmployeeService.java
@@ -1,7 +1,26 @@
 package com.konrad.employeemanagement.employee;
 
+import com.konrad.employeemanagement.employee.domain.Employee;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public Employee getEmployeeById(long id){
+        return employeeRepository.getById(id);
+    }
+
+    public Employee getEmployeeByEmail(String email){
+        return employeeRepository.getEmployeeByEmail(email);
+    }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
