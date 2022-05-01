43d50aad2fa02dd399f6217820beefa6228a3311
konradsiekacz <konradsiekacz@gmail.com>
2022-05-01 21:02:19 +0200

    add crud method for Employee

--- src/main/java/com/konrad/employeemanagement/employee/EmployeeService.java
+++ src/main/java/com/konrad/employeemanagement/employee/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.konrad.employeemanagement.employee;
 
 import com.konrad.employeemanagement.employee.domain.Employee;
+import com.konrad.employeemanagement.employee.dto.EmployeeDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -12,15 +14,35 @@ public class EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    public Employee getEmployeeById(long id){
+    public Employee getEmployeeById(long id) {
         return employeeRepository.getById(id);
     }
 
-    public Employee getEmployeeByEmail(String email){
+    public Employee getEmployeeByEmail(String email) {
         return employeeRepository.getEmployeeByEmail(email);
     }
 
-    public List<Employee> getAllEmployees(){
-        return employeeRepository.findAll();
+
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeRepository.findAll().stream()
+                .map(EmployeeConverter::entityToDTO)
+                .collect(Collectors.toList());
     }
+
+    public Employee addEmployee(EmployeeDTO employeeDTO) {
+        return employeeRepository.save(EmployeeConverter.DTOtoEntity(employeeDTO));
+    }
+
+    public void updateEmployee(Employee updateEmployee) {
+        Employee employee = getEmployeeById(updateEmployee.getId());
+        employee.setFirstName(updateEmployee.getFirstName());
+        employee.setLastName(updateEmployee.getLastName());
+        employee.setEmail(updateEmployee.getEmail());
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(long id) {
+        employeeRepository.deleteById(id);
+    }
+
 }
