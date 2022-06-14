e0f1f7921cd4f81ebf9020928cda6ee55098f12f
konradsiekacz <konradsiekacz@gmail.com>
2022-06-14 22:57:11 +0200

    add annotation for column and constructor

--- src/main/java/com/konrad/employeemanagement/employee/domain/Employee.java
+++ src/main/java/com/konrad/employeemanagement/employee/domain/Employee.java
@@ -1,16 +1,25 @@
 package com.konrad.employeemanagement.employee.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "employee")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @Column(name = "id")
     private long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
 }
