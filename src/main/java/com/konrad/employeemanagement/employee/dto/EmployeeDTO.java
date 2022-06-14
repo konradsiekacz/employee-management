3dc1ff2c0c7dd0b6198a19710c558747ee0bf15a
konradsiekacz <konradsiekacz@gmail.com>
2022-06-14 22:58:16 +0200

    add json property for fields

--- src/main/java/com/konrad/employeemanagement/employee/dto/EmployeeDTO.java
+++ src/main/java/com/konrad/employeemanagement/employee/dto/EmployeeDTO.java
@@ -1,13 +1,24 @@
 package com.konrad.employeemanagement.employee.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Builder
 public class EmployeeDTO {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @JsonProperty("id")
     private long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+    @JsonProperty("email")
     private String email;
 }
