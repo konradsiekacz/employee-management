31241f03c9b4f790929ee7913b80fce661321b31
konradsiekacz <konradsiekacz@gmail.com>
2022-06-14 22:57:45 +0200

    add post method

--- src/main/java/com/konrad/employeemanagement/employee/EmployeeController.java
+++ src/main/java/com/konrad/employeemanagement/employee/EmployeeController.java
@@ -3,6 +3,7 @@ package com.konrad.employeemanagement.employee;
 import com.konrad.employeemanagement.employee.domain.Employee;
 import com.konrad.employeemanagement.employee.dto.EmployeeDTO;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +14,6 @@ import java.util.List;
 @AllArgsConstructor
 public class EmployeeController {
     private EmployeeService employeeService;
-
-
     @GetMapping("")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
@@ -27,5 +26,12 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.OK)
     public EmployeeDTO getAllEmployees(@PathVariable("id") int id) {
         return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping("")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addEmployee(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.addEmployee(employeeDTO);
     }
 }
