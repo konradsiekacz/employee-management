c3f66082eb5a4564910e3864093329696d30b8c4
konradsiekacz <konradsiekacz@gmail.com>
2022-06-16 22:51:50 +0200

    add put method to update employee

--- src/main/java/com/konrad/employeemanagement/employee/EmployeeController.java
+++ src/main/java/com/konrad/employeemanagement/employee/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.konrad.employeemanagement.employee;
+import com.konrad.employeemanagement.employee.domain.Employee;
 import com.konrad.employeemanagement.employee.dto.EmployeeDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/employees")
@@ -18,7 +21,7 @@ public class EmployeeController {
         return employeeService.getAllEmployees();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public EmployeeDTO getAllEmployees(@PathVariable("id") Long id) {
@@ -30,5 +33,19 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public void addEmployee(@RequestBody EmployeeDTO employeeDTO){
         employeeService.addEmployee(employeeDTO);
+    }
+
+    @PutMapping()
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public void updateEmployee(@RequestBody Employee employee){
+        employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable("id")Long id){
+        employeeService.deleteEmployee(id);
     }
 }
